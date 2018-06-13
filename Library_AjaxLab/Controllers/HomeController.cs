bb5c438617cc0e98de686b88b46b4229c27587f6
kman12681 <keith.rigling@gmail.com>
2018-06-13 09:10:28 -0400

    adding linq samples

--- Library_AjaxLab/Controllers/HomeController.cs
+++ Library_AjaxLab/Controllers/HomeController.cs
@@ -36,15 +36,13 @@ namespace Library_AjaxLab.Controllers
             LibraryEntities db = new LibraryEntities();
 
             //LINQ version:
-            /*List<Customer> list = (
-                from c in db.Customers
-                where c.City == city
-                select c
+            /*List<book> list = (
+                from b in db.books
+                where b.Author.Contains(author)
+                select b
                 ).ToList();*/
-            //exact same thing in a Lambda expression
-            /*List<Customer> list = db.Customers.Where(
-                c=>c.City.Equals(city)
-                ).ToList();*/
+
+            //exact same thing in a Lambda expression           
             List<book> list = db.books.Where(
                b => b.Author.Contains(author)
                 ).ToList();
@@ -57,15 +55,13 @@ namespace Library_AjaxLab.Controllers
             LibraryEntities db = new LibraryEntities();
 
             //LINQ version:
-            /*List<Customer> list = (
-                from c in db.Customers
-                where c.City == city
-                select c
+            /*List<book> list = (
+                from b in db.books
+                where b.Title.Contains(title)
+                select b
                 ).ToList();*/
-            //exact same thing in a Lambda expression
-            /*List<Customer> list = db.Customers.Where(
-                c=>c.City.Equals(city)
-                ).ToList();*/
+
+            //exact same thing in a Lambda expression            
             List<book> list = db.books.Where(
                b => b.Title.Contains(title)
                 ).ToList();
@@ -78,15 +74,13 @@ namespace Library_AjaxLab.Controllers
             LibraryEntities db = new LibraryEntities();
 
             //LINQ version:
-            /*List<Customer> list = (
-                from c in db.Customers
-                where c.City == city
-                select c
+            /*List<book> list = (
+                from b in db.books
+                where b.YearPublished == year
+                select b
                 ).ToList();*/
-            //exact same thing in a Lambda expression
-            /*List<Customer> list = db.Customers.Where(
-                c=>c.City.Equals(city)
-                ).ToList();*/
+
+            //exact same thing in a Lambda expression            
             List<book> list = db.books.Where(
                b => b.YearPublished == year
                 ).ToList();
